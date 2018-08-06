d7dd33b3330680a5f1b9180aae3969f3cb2af504
LucasMoretti <lucasmoretti8@gmail.com>
2018-08-06 11:21:51 -0300

    Inserção do teste de remoção

--- Alpha-project/src/Teste.java
+++ Alpha-project/src/Teste.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.junit.Test;
 import java.util.concurrent.TimeUnit;
 
+import static junit.framework.TestCase.assertTrue;
+
 public class Teste {
 
     @Test
@@ -27,11 +29,44 @@ public class Teste {
 
         driver.findElement(By.xpath("//a[@href='/tenis-asics-gel-equation-9-masculino-D18-0650-090-42']")).click();
 
-        driver.findElement(By.id("buy-button-now")).click();
-
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
+        driver.findElement(By.id("buy-button-now")).click();
 
+        assertTrue(driver.getPageSource().contains("Continuar comprando"));
+
+    }
+
+
+    @Test
+    public void deveLimparCarrinho() {
+
+        System.setProperty("webdriver.gecko.driver", "C:\\Users\\Lucas\\Documents\\Coding\\Selenium\\geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+
+        driver.get("https://www.netshoes.com.br/");
+
+        WebElement query = driver.findElement(By.name("q"));
+        query.sendKeys("Asics");
+        query.submit();
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        driver.findElement(By.linkText("Tênis Asics Gel Equation 9 Masculino")).click();
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        driver.findElement(By.xpath("//a[@href='/tenis-asics-gel-equation-9-masculino-D18-0650-090-42']")).click();
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        driver.findElement(By.id("buy-button-now")).click();
+
+        driver.findElement(By.xpath("//a[@href='/cart']")).click();
+
+        driver.findElement(By.linkText("Remover")).click();
+
+        assertTrue(driver.getPageSource().contains("Seu carrinho está vazio"));
 
     }
 }
