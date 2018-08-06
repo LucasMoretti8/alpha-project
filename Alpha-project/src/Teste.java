2e876d91643b420872c7e7694332f521fe6b6165
LucasMoretti <lucasmoretti8@gmail.com>
2018-08-06 17:08:46 -0300

    Inserção de mais dois cenários

--- Alpha-project/src/Teste.java
+++ Alpha-project/src/Teste.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.junit.Test;
 import java.util.concurrent.TimeUnit;
 
+
 import static junit.framework.TestCase.assertTrue;
 
 public class Teste {
@@ -68,5 +69,50 @@ public class Teste {
 
         assertTrue(driver.getPageSource().contains("Seu carrinho está vazio"));
 
+    }
+
+    @Test
+    public void deveContinuarComprando(){
+
+        System.setProperty("webdriver.gecko.driver", "C:\\Users\\Lucas\\Documents\\Coding\\Selenium\\geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+
+        driver.get("https://www.netshoes.com.br/");
+
+        WebElement query = driver.findElement(By.name("q"));
+        query.sendKeys("Asics");
+        query.submit();
+
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+        driver.findElement(By.linkText("Tênis Asics Gel Equation 9 Masculino")).click();
+
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+        driver.findElement(By.xpath("//a[@href='/tenis-asics-gel-equation-9-masculino-D18-0650-090-42']")).click();
+
+        driver.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
+
+        driver.findElement(By.id("buy-button-now")).click();
+
+        driver.findElement(By.xpath("//a[@href='/cart']")).click();
+
+        driver.findElement(By.linkText("Continuar comprando")).click();
+
+        WebElement query2 = driver.findElement(By.name("q"));
+        query2.sendKeys("Mochila Wilson");
+        query2.submit();
+
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+        driver.findElement(By.linkText("Mochila Wilson Esportiva WTIX13551A")).click();
+
+        driver.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
+
+        driver.findElement(By.id("buy-button-now")).click();
+
+        driver.findElement(By.xpath("//a[@href='/cart']")).click();
+
+        assertTrue(driver.getPageSource().contains("Continuar comprando"));
     }
 }
